dd0fe02bd5c6e4dea38a4029e2bc026ea0b77fda
Arthur Hoek <arthur@arthurhoek.nl>
2024-12-04 22:43:07 +0100

    Day 3

---
+++ 3/task-1.mjs
@@ -0,0 +1,16 @@
+// Read data
+import fs from 'node:fs'
+const input = 'input';
+// const input = 'demo';
+const data = fs.readFileSync(`./3/${input}.txt`, 'utf8')
+
+let result = 0;
+// console.log(data);
+const matches = data.match(/mul\([0-9]+,[0-9]+\)/g);
+console.log(matches)
+matches.forEach(m => {
+  const cs = m.replace('mul(', '').replace(')', '')
+  const [a,b] = cs.split(',')
+  result += parseInt(a, 10) * parseInt(b, 10)
+})
+console.log(result);
---
+++ 3/task-2.mjs
@@ -0,0 +1,25 @@
+import fs from 'node:fs';
+
+const input = 'input';
+// const input = 'demo2';
+const data = fs.readFileSync(`./3/${input}.txt`, 'utf8');
+
+let result = 0;
+let enabled = true;
+
+const matches = data.match(/mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)/g);
+
+matches.forEach(m => {
+  if (m.startsWith('mul')) {
+    if (enabled) {
+      const [_, a, b] = m.match(/mul\((\d+),(\d+)\)/);
+      result += parseInt(a) * parseInt(b);
+    }
+  } else if (m === 'do()') {
+    enabled = true;
+  } else if (m === 'don\'t()') {
+    enabled = false;
+  }
+});
+
+console.log(result);
